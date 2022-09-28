f176cf3a728cfeead520abc0bf677574e1937450
StephanieSander <slovanieslovanie@gmail.com>
2022-09-28 09:20:08 +0100

    add english link

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/flyer-iuc'),
+      dir: require('path').join(__dirname, './coverage/dasa-iuc'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FlyerIUC'`, () => {
+  it(`should have as title 'DasaIUC'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FlyerIUC');
+    expect(app.title).toEqual('DasaIUC');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FlyerIUC app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('DasaIUC app is running!');
   });
 });
